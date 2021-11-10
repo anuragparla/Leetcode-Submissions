0479c0950d09f58308d0b4c52df351f5e099a5f7
Anurag Parla <anuragparla@gmail.com>
2021-11-09 23:40:09 -0500

    Added an optimal solution by using HashSet

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
+        /*
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++){
                 if(arr[i]==2*arr[j] || arr[j]==2*arr[i]){
@@ -9,6 +10,14 @@ class Solution {
             
         }
         return false;
-        
+        */
+         HashSet hs = new HashSet();
+        for(int i=0;i<arr.length;i++){
+            if(hs.contains(arr[i]*2) || (arr[i]%2==0 && hs.contains(arr[i]/2))){
+                return true;
+            }
+            hs.add(arr[i]);
+        }
+        return false;
     }
 }
