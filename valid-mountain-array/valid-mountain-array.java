6255e59a25d00500b87bdcab4f9402f86b153176
Anurag Parla <anuragparla@gmail.com>
2021-11-11 00:05:06 -0500

    Time: 1 ms (100.00%), Space: 40 MB (77.21%) - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        
+        int i=0;
+        
+        while(i<arr.length-1 && arr[i]<arr[i+1]){
+            i+=1;
+        }
+        
+        if(i==0 || i==arr.length-1){
+            return false;
+        }
+        
+        while(i<arr.length-1 && arr[i]>arr[i+1]){
+            i+=1;
+        }
+        
+        return i == arr.length-1;
+    }
+}
