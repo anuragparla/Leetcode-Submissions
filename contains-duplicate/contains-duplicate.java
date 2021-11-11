4b697d16db19b3df19b76f9a535f3bd96fb910f2
Anurag Parla <anuragparla@gmail.com>
2021-11-11 00:11:56 -0500

    Time: 6 ms (66.43%), Space: 45 MB (64.54%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set hs = new HashSet();
+        for(int i=0;i<nums.length;i++){
+            if(hs.contains(nums[i]))
+                return true;
+            else hs.add(nums[i]);
+        }
+        return false;
+    }
+}
