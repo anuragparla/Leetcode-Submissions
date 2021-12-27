9a840c61a58ae26714170872022bce50ee0efaae
Anurag Parla <anuragparla@gmail.com>
2021-12-27 11:48:25 -0500

    Updated with the optima solution having O(1

--- product-of-array-except-self/product-of-array-except-self.java
+++ product-of-array-except-self/product-of-array-except-self.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        /* O(N)T O(N)S 
         int[] prefix_sum = new int[nums.length];
         int[] postfix_sum = new int[nums.length];
         int[] res = new int[nums.length];
@@ -27,8 +28,25 @@ class Solution {
             else {
                 res[i] = prefix_sum[i-1];
             }
-        }
+        }*/
         
+        /**
+        O(N)T O(1)S
+        */
+        int[] res = new int[nums.length];
+        // calculating the prefix till last but one element and storing it in the output array
+        int prefix = 1;
+        res[0] = prefix;
+        for(int i = 0; i<nums.length-1;i++) {
+            prefix = prefix*nums[i];
+            res[i+1] = prefix;
+        }
+        // calculating the postfix from nums[length-2] element and directly multiplying with the content of the output array which is the prefixes 
+        int postfix = nums[nums.length-1];
+        for(int j=nums.length-2;j>=0;j--) {
+            res[j]=res[j]*postfix;
+            postfix = postfix*nums[j]; 
+        }
         return res;
     }
 }
