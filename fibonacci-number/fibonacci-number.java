62f14643c24a29460d613a931e85867203c83caf
Anurag Parla <anuragparla@gmail.com>
2022-01-02 18:08:27 -0500

    Time: 0 ms (100.00%), Space: 37.6 MB (14.35%) - LeetHub

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,13 +1,21 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0) {
-            return 0;
-        }
-        else if (n == 1) {
-            return 1;
+        /*
+        if(n <= 1) {
+            return n;
         }
         else {
             return fib(n-2)+fib(n-1);
+        }*/
+    int[] fibArr = new int[n+1];
+        if (n > 0) {
+            fibArr[1] = 1;
         }
-    }
+        if (n > 1) {
+            for (int i = 2; i < n+1; i++) {
+                fibArr[i] = fibArr[i-1] + fibArr[i-2];
+            }
+        }
+    return fibArr[n];
 }
+    }
