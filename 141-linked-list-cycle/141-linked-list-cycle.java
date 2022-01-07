ccf807d285bfa4cf02d008fe3cf84d6561e35da6
Anurag Parla <anuragparla@gmail.com>
2022-01-07 00:27:48 -0500

    Time: 0 ms (100.00%), Space: 39.9 MB (71.44%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode hare;
+        ListNode tortoise;
+        hare = tortoise = head;
+        while(hare != null && hare.next != null) {
+            
+            hare = hare.next.next;
+            tortoise = tortoise.next;
+            
+            if(hare == tortoise) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
