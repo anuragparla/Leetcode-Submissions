28b816609bd1558f4d3d86b260ed163d1d4a4503
Anurag Parla <anuragparla@gmail.com>
2022-01-11 00:05:28 -0500

    Time: 2 ms (22.81%), Space: 52.7 MB (11.67%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -4,10 +4,8 @@ class Solution {
         int j = s.length-1;
         while ( i< j) {
             char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i+= 1;
-            j-=1;
+            s[i++] = s[j];
+            s[j--] = temp;
         }
     }
 }
