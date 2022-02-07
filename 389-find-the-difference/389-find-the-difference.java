3c323462cc4ecc9366ea4f974eb02953b86ffc1a
Anurag Parla <anuragparla@gmail.com>
2022-02-07 00:15:54 -0500

    Time: 2 ms (75.61%), Space: 40.4 MB (18.78%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public char findTheDifference(String s, String t) {
+       char[] sArray = s.toCharArray();
+       char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        for(int i = 0; i<sArray.length; i++) {
+            if(sArray[i] == tArray[i])
+                continue;
+            else {
+                return tArray[i];
+            }
+        }
+        return tArray[tArray.length-1];
+    }
+}
