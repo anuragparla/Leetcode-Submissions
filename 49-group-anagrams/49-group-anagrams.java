ad0d53a662de420c4871e723014a3231f9e451a2
Anurag Parla <anuragparla@gmail.com>
2022-03-23 23:20:38 -0400

    Time: 36 ms (17.89%), Space: 65.1 MB (9.18%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         //approach one 
+        /*
         Map<String,List<String>> hm = new HashMap<>();
         List<List<String>> res = new ArrayList<>();
         for(int i = 0; i<strs.length; i++) {
@@ -9,13 +10,28 @@ class Solution {
             Arrays.sort(temp);
             String k = new String(temp);
             if(hm.containsKey(k))
-            {
                 hm.get(k).add(strs[i]);
-            }
             else
                 hm.put(k,new ArrayList(Arrays.asList(strs[i])));
         }
         for(List<String> val:hm.values()) {
+            res.add(val);
+        } 
+        return res;*/
+        Map<String,List<String>> hm = new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+        for(int i = 0; i<strs.length; i++) {
+            int[] count = new int[26];
+            for(int j = 0; j<strs[i].length(); j++) {
+                count[strs[i].charAt(j)-97] += 1;
+            }
+            String k = Arrays.toString(count);
+            if(hm.containsKey(k))
+                hm.get(k).add(strs[i]);
+            else
+                hm.put(k,new ArrayList(Arrays.asList(strs[i])));
+        }
+        for(List<String> val : hm.values()) {
             res.add(val);
         }
         return res;
