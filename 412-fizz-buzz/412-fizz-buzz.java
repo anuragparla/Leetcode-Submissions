0a4b209b8164b7018982db908001ca0a7669f814
Anurag Parla <anuragparla@gmail.com>
2022-07-13 22:43:48 -0400

    Time: 2 ms (73.05%), Space: 48.6 MB (32.34%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.java
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -2,26 +2,20 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> answer = new ArrayList<>();
         String temp;
-        
-        
         for(int i =1; i<=n; i++) {
              if( i % 3 == 0 && i % 5 == 0) {
             temp = "FizzBuzz";
-            answer.add(temp);
         }
             else if (i % 3 == 0) {
             temp = "Fizz";
-            answer.add(temp);
         }
         else if (i % 5 == 0) {
             temp = "Buzz";
-            answer.add(temp);
         }
         else {
             temp = Integer.toString(i);
-            answer.add(temp);
         }
-            
+           answer.add(temp); 
         }
         return answer;
     }
