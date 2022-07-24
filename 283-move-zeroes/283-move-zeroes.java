b2cfcde8450d9ea238eb14451e7131188a7c3d62
Anurag Parla <anuragparla@gmail.com>
2022-07-24 17:28:20 -0400

    Time: 2 ms (80.93%), Space: 55.2 MB (13.62%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,20 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // using two pointer 2 pick and place the number
-        int j = 0;
-        for(int i = 0; i<nums.length;i++) {
-            if(nums[i] != 0)
-            {
-                nums[j] = nums[i];
-                j++;
+        int nonZeroFoundAt = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[nonZeroFoundAt] = nums[i];
+                nonZeroFoundAt += 1;
             }
-                
-            
-         }
-        while(j < nums.length)
-        {
-            nums[j] = 0;
-            j++;
+        }
+        for(int j = nonZeroFoundAt; j< nums.length; j++) {
+            nums [j] = 0;
         }
     }
 }
