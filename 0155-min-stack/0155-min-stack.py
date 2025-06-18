ba4e98ba60c0b255e16b70b583ec4735c91d84cb
Anurag Parla <anuragparla@gmail.com>
2025-06-18 07:16:58 -0400

    Time: 7 ms (50.99%), Space: 21.4 MB (41.88%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,33 +2,33 @@ class MinStack:
 
     def __init__(self):
         self.minStack = []
-        self.normalStack = []
-        self.minValue = 0
+        self.minValue = float('inf')
 
     def push(self, val: int) -> None:
-        if len(self.normalStack) == 0:
-            self.minValue = val
-        else:
+        # when stack is empty, 1st value pushed is the min val
+        if val<=self.minValue:
+            self.minStack.append(self.minValue)
             self.minValue = min(self.minValue,val)
-        
-        self.normalStack.append(val)
-        self.minStack.append(self.minValue)
+            self.minStack.append(val)
+        else:
+            self.minStack.append(val)
     
     def pop(self) -> None:
-        if len(self.normalStack) != 0 and len(self.minStack) != 0:
-            self.normalStack.pop()
-            self.minStack.pop()
-            if len(self.minStack) != 0:
-                self.minValue = self.minStack[-1]
+        # always check if stack is empty before popping
+            popped = self.minStack.pop()
+            if popped == self.minValue:
+                self.minValue = self.minStack.pop()
+
+        # after popping, make sure stack not empty before accessing
+            # if len(self.minStack) != 0:
+            #     self.minValue = self.minStack[-1]
 
     def top(self) -> int: 
-        if len(self.normalStack) != 0:
-            return self.normalStack[-1]
+        if len(self.minStack) > 0:
+            return self.minStack[-1]
     
     def getMin(self) -> int:
-        if len(self.minStack) != 0:
-           
-            return self.minStack[-1]
+        return self.minValue
         
 
 
