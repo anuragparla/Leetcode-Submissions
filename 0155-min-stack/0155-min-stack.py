93ccbcad91f5089efac3b513cc403d3263a22147
Anurag Parla <anuragparla@gmail.com>
2025-06-18 07:20:25 -0400

    Time: 0 ms (100%), Space: 21.4 MB (63.36%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,3 +1,20 @@
+'''
+1) push()
+Time complexity: O(1)
+Space complexity: O(1)
+
+2) pop()
+Time complexity: O(1)
+Space complexity: O(1)
+
+3) top()
+Time complexity: O(1)
+Space complexity: O(1)
+
+4) getMin()
+Time complexity: O(1)
+Space complexity: O(1)
+'''
 class MinStack:
 
     def __init__(self):
@@ -5,7 +22,6 @@ class MinStack:
         self.minValue = float('inf')
 
     def push(self, val: int) -> None:
-        # when stack is empty, 1st value pushed is the min val
         if val<=self.minValue:
             self.minStack.append(self.minValue)
             self.minValue = min(self.minValue,val)
@@ -14,14 +30,9 @@ class MinStack:
             self.minStack.append(val)
     
     def pop(self) -> None:
-        # always check if stack is empty before popping
             popped = self.minStack.pop()
             if popped == self.minValue:
                 self.minValue = self.minStack.pop()
-
-        # after popping, make sure stack not empty before accessing
-            # if len(self.minStack) != 0:
-            #     self.minValue = self.minStack[-1]
 
     def top(self) -> int: 
         if len(self.minStack) > 0:
