86f8ae635ef9841c2c209128da0e447c8d64f1c2
Anurag Parla <anuragparla@gmail.com>
2025-06-24 11:58:05 -0400

    Time: 34 ms (96.51%), Space: 19.1 MB (27.33%) - LeetHub

--- 0786-search-in-a-sorted-array-of-unknown-size/0786-search-in-a-sorted-array-of-unknown-size.py
+++ 0786-search-in-a-sorted-array-of-unknown-size/0786-search-in-a-sorted-array-of-unknown-size.py
@@ -9,9 +9,9 @@ class Solution:
     def search(self, reader: 'ArrayReader', target: int) -> int:
         low = 0
         high = 1
-        while reader.get(high) < target:
-            low = high
-            high *= 2
+        # while reader.get(high) < target:
+        #     low = high
+        #     high *= 2
 
         while low <= high:
             mid = low + (high - low) // 2
@@ -21,5 +21,6 @@ class Solution:
             elif val > target or val == 2**31 - 1:
                 high = mid - 1
             else: 
+                high *= 2
                 low = mid + 1
         return -1
