5f037eb07f01469444f7fa46d7be67aa82e83540
Anurag Parla <anuragparla@gmail.com>
2025-06-29 18:44:27 -0400

    Time: 0 ms (100%), Space: 18.1 MB (31.61%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,36 +1,23 @@
+from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        '''
-        since the array is sorted, rotating the array 
-        will result in 2 halfs with each being sorted 
-        i.e. 1 half would have larger values and the 
-        other would have smaller depending on how many
-        times the array has been rotated
-
-        do a regular BS and find the mid. 
-        if mid >= left most element then we need to move
-        left = mid +1 
-        if mid <= left most element then we need to move 
-        right = mid - 1 
-
-        PS: this logic will only work for rotated sorted array
-
-        '''
-        left, right = 0, len(nums)-1
-        minVal = nums[0]
-        while left<=right:
-            mid = left + (right-left)//2
-            if nums[left]<nums[right]:
-                minVal = min(minVal, nums[left])
-                break
-            
-            elif nums[mid] >=nums[left]:
-                minVal = min(minVal, nums[mid])
-                left = mid +1 
-            else:
-                minVal = min(minVal, nums[mid])
-                right = mid -1 
-        return minVal
+       if len(nums) == 1:
+        return nums[0]
+       
+       low = 0
+       high = len(nums) - 1
+       while low<=high:
+        if nums[low] <= nums[high]:
+            return nums[low]
+        
+        mid = low + (high - low)//2
+        if (mid ==0 or nums[mid] < nums[mid - 1]) and (mid == len(nums) - 1 or nums[mid]<nums[mid + 1]):
+            return nums[mid]
+        
+        if nums[low]<=nums[mid]:
+            low = mid + 1 
+        else:
+            high = mid - 1
         
 
         
