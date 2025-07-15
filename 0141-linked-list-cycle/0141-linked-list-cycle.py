5aca732b27cd41dc2ff30cafad775df724887873
Anurag Parla <anuragparla@gmail.com>
2025-07-14 21:16:04 -0400

    Time: 55 ms (17.39%), Space: 19.8 MB (56.84%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,44 +6,10 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        '''
-        I am assuming i'll use hashmap to keep track of node
-        if the .next is a value in the hashmap
-         it means there is a cycle
-        return true 
-        if the while loop exits then it means there is .next = None which means there 
-        is no cycle
-        Time: O(n)
-        Space: O(n)
-        '''
-        # curr = head
-        # valueIndexMap = {}
-        # while curr != None:
-        #     if curr not in valueIndexMap:
-        #         valueIndexMap[curr] = curr
-        #     if curr.next in valueIndexMap:
-        #         return True
-        #     curr = curr.next
-        # return False
-
-
-        '''
-        Improved solution: Flyod Marhsall's algorithm also called hare and tortoise
-        Time: O(n)
-        Space: O(1)
-        slow and fast pointers. slow moves by 1 and fast moves by two. 
-        if both meet then its a cycle but if fast reaches none then no cycle 
-        '''
-        fast, slow = head, head 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next 
+        slow , fast = head , head
+        while fast is not None and fast.next is not None:
+            slow = slow.next 
+            fast = fast.next.next
             if slow == fast:
                 return True
-        return False 
-    
-
-
-
-
-        
+        return False
