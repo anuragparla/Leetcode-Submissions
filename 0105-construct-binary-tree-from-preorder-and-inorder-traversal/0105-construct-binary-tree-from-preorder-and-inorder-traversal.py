2ec7f383cf96eadf8971c648cb99fb7c852126a1
Anurag Parla <anuragparla@gmail.com>
2025-07-20 18:12:29 -0400

    Time: 0 ms (100%), Space: 19.1 MB (92.29%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,10 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    mapp = {}
+    idx = 0
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if preorder is None or len(preorder) == 0 or inorder is None or len(inorder) == 0:
             return None
         
+        for i in range(len(inorder)):
+            self.mapp[inorder[i]] = i 
+        
+        return self.createTree(preorder, 0, len(inorder)-1)
+    
+    def createTree(self, preorder, start, end):
+        if start > end:
+            return None 
+        rootVal = preorder[self.idx]
+        self.idx += 1
+        root = TreeNode(rootVal)
+        rootIdx = self.mapp[rootVal]
+        root.left = self.createTree(preorder, start, rootIdx - 1)
+        root.right = self.createTree(preorder, rootIdx + 1, end)
+        return root  
+        '''
+        Recursive method causes the time complexity to be O(N^2)
         root = TreeNode(preorder[0])
         rootIdx = -1 
         for i in range(len(inorder)):
@@ -21,5 +40,6 @@ class Solution:
         root.left = self.buildTree(preorderLeft, inorderLeft)
         root.right = self.buildTree(preorderRight,inoderRight )
         return root
+        '''
 
         
