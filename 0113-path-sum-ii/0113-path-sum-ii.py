bcd743a622f57e9200b0a13a00423a9f40aad80e
Anurag Parla <anuragparla@gmail.com>
2025-07-22 14:32:51 -0400

    Time: 28 ms (5.55%), Space: 18.9 MB (73.29%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -12,7 +12,10 @@ class Solution:
         self.res = []
         self.dfs(root,0,targetSum, list())
         return self.res
-    
+    '''
+    This approach will result in 
+    Time complexity: O(NH) where H is the number of nodes to be copied into new list 
+    Space complexity: O(NH)
     def dfs(self, root, currSum, targetSum, path):
         if root is None:
             return
@@ -29,5 +32,24 @@ class Solution:
         self.dfs(root.left, currSum, targetSum, new_path)
         
         self.dfs(root.right, currSum, targetSum, new_path)
+    '''
+    #OPTIMIZED APPROACH 
+    def dfs(self, root, currSum, targetSum, path):
+        if root is None:
+            return 
+        currSum += root.val
+        print('currSum', currSum)
+        path.append(root.val)
+        print(path)
+        
+        self.dfs(root.left, currSum, targetSum, path)
+        if root.left is None and root.right is None:
+            if currSum == targetSum:
+                new_path = path[:]
+                print('successful paths')
+                print(new_path)
+                self.res.append(new_path)
+        self.dfs(root.right, currSum, targetSum, path)
+        path.pop(len(path)-1)
 
         
