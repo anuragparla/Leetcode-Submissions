0d3843391f28bb41d0e5437d7115bd73d4a56fe2
Anurag Parla <anuragparla@gmail.com>
2025-08-27 07:32:27 -0400

    Time: 79 ms (95.05%), Space: 26.8 MB (91.44%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,13 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        result = [0] * len(temperatures)
+        res = [0] * len(temperatures)
         mono_stack = []
 
         for i in range(len(temperatures)):
+            # we compare the temperature at the top of the stack with the next greatest temp we found
             while mono_stack and temperatures[i]>temperatures[mono_stack[-1]]:
-                idx = mono_stack.pop()
-                days = i - idx
-                result[idx] = days
+                value_popped = mono_stack.pop()
+                res[value_popped] = i - value_popped
+            # if stack is empty or the next temp is lesser than the top of the stack
             mono_stack.append(i)
-        return result
+        #for days where they will not have next higher temp
+        while mono_stack:
+            res[mono_stack.pop()] = 0
+        return res
         
