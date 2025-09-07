22746e0710441d1eefb9289ceb0f3939ef517d77
Anurag Parla <anuragparla@gmail.com>
2025-09-07 17:18:45 -0400

    Time: 3 ms (40.07%), Space: 17.8 MB (45.63%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,35 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        '''
+        we need count of each char 
+        every char can be inserted into a map and its value is freq
+        a = 1 b = 1 c = 4 , d = 2 
+        if the frequency is even then they all will be part of the palindrome string 
+        if frequency is odd, then only one of those odds can be used as the center char 
+        '''
+        if len(s) == 1:
+            return 1
+        frequency_map = dict()
+        one_count = False
+        odd_occurence = False
+        max_palindrome_length = 0
+        for i in s:
+            if i not in frequency_map:
+                frequency_map[i] = 1
+            else:
+                frequency_map[i] += 1
+        if len(frequency_map) == 1:
+            return list(frequency_map.values())[0]
+        for key in frequency_map:
+            if frequency_map[key] % 2 == 0:
+                max_palindrome_length += frequency_map[key]
+            elif frequency_map[key] > 1:
+                if not odd_occurence:
+                    max_palindrome_length += frequency_map[key]
+                    odd_occurence = True
+                else:
+                    max_palindrome_length += frequency_map[key] - 1
+            elif not odd_occurence and frequency_map[key] == 1 :
+                max_palindrome_length += 1 
+                odd_occurence = True
+        return max_palindrome_length
