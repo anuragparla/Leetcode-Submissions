655a8e2b6a8a300e1842955026431958c833c150
Anurag Parla <anuragparla@gmail.com>
2025-09-07 15:35:05 -0400

    Time: 20 ms (5.6%), Space: 18.1 MB (36.35%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -7,21 +7,41 @@ class Solution:
         Time: O(N)
         Space: O(N)
         '''
-        s_to_t_map = dict()
-        set_of_t_chars = set()
+        # s_to_t_map = dict()
+        # set_of_t_chars = set()
 
-        for i in range(len(s)):
-            if s[i] not in s_to_t_map:
-                if t[i] not in set_of_t_chars:
-                    s_to_t_map[s[i]] = t[i]
-                    set_of_t_chars.add(t[i])
-                else:
-                    return False
-            else:
-                if s_to_t_map[s[i]] != t[i]:
-                    return False
+        # for i in range(len(s)):
+        #     if s[i] not in s_to_t_map:
+        #         if t[i] not in set_of_t_chars:
+        #             s_to_t_map[s[i]] = t[i]
+        #             set_of_t_chars.add(t[i])
+        #         else:
+        #             return False
+        #     else:
+        #         if s_to_t_map[s[i]] != t[i]:
+        #             return False
         
+        # return True
+        '''
+        Method 2: Using Single Hashmap
+        Time: O(N)
+        Space: O(N)
+        '''
+        char_to_char_map = dict()
+        for i in range(len(s)):
+            if s[i]+'_s' not in char_to_char_map:
+                char_to_char_map[s[i]+'_s'] = t[i]
+            else:
+                if char_to_char_map[s[i]+'_s'] != t[i]:
+                    return False
+
+            if t[i]+'_t' not in char_to_char_map:
+                char_to_char_map[t[i]+'_t'] = s[i]
+            else:
+                if char_to_char_map[t[i]+'_t'] != s[i]:
+                    return False
         return True
+
 
 
 
