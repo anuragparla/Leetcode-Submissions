f7177d9bbfc0d2062856066e891697a324ed2881
Anurag Parla <anuragparla@gmail.com>
2025-10-06 22:58:43 -0400

    Time: 23 ms (63.01%), Space: 23.2 MB (80.17%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,5 +1,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        '''
         prefix_product = 1 
         #prefix product for lhs of an element
         lhs=[1]*len(nums)
@@ -18,4 +19,17 @@ class Solution:
             rhs[i] = lhs[i] * rhs[i]
         
         return rhs
-
+        '''
+        #optimzied solution 
+        res = [1] * len(nums)
+        prefix_product = 1 
+        # calculate the products on LHS
+        for i in range(1,len(nums)):
+            res[i] = nums[i-1] * prefix_product
+            prefix_product = res[i]
+        prefix_product = 1 
+        # use result array and modify it for rhs prod calc
+        for j in range(len(nums)-2, -1,-1):
+            res[j] = res[j] * nums[j+1] * prefix_product
+            prefix_product = nums[j+1] * prefix_product
+        return res
