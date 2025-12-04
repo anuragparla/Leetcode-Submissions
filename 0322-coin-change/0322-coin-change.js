691118b69d1fc56ce804a06015736b6250bc42d2
Anurag Parla <anuragparla@gmail.com>
2025-12-03 20:50:40 -0500

    Time: 118 ms (17.48%), Space: 61.9 MB (31.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -7,33 +7,34 @@ var coinChange = function(coins, amount) {
     if (!coins || coins.length === 0){
         return -1
     }
-    const result = recurse(coins,0, amount,{})
+    
+    const result = recurse(coins,amount,{})
     return result === Infinity ? -1 : result
-
-    function recurse(coins,index,amount,memo){
-        const key = index + '-'+ amount
+    function recurse(coins,amount,memo){
+        
+        if (amount in memo ) {
+            return memo[amount]
+        }
 
         if (amount === 0){
             return 0
         }
 
-        if(amount <0 || index >= coins.length){
+        if (amount < 0){
             return Infinity
         }
+        let minCoins = Infinity
 
-        if (key in memo){
-            return memo[key]
+        for (let coin of coins){
+            const res = recurse(coins,amount - coin, memo) 
+            if (res !== Infinity){
+
+            minCoins = Math.min(minCoins, 1+res)
+            }
         }
 
-        const coinNotChosen = recurse(coins,index+1,amount, memo)
-
-        const  coinChosen = recurse(coins, index, amount -coins[index], memo)
-
-        const ifChoosing = coinChosen === Infinity ? Infinity : 1+coinChosen
-
-        memo[key] = Math.min(coinNotChosen, ifChoosing)
-
-        return memo[key]
+        memo[amount] = minCoins
+        return minCoins
+          
     }
-
 };
