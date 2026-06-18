b550b9c1fef817ad2693073789241b15b4c9a189
Anurag Parla <anuragparla@gmail.com>
2026-06-18 11:52:53 -0400

    Time: 9 ms (20.43%), Space: 20.4 MB (61.59%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,29 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        '''
+        if left and right are 0 only inc right
+        if left is 0 and right is != 0 swap and inc both pointers 
+        '''
+        left = 0 
+        right = 1 
+        if len(nums)> 1:
+            while left < len(nums) and right < len(nums):
+                if nums[left] == 0 and nums[right] != 0:
+                    nums[left],nums[right] = nums[right],nums[left]
+                    left +=1 
+                    right +=1 
+                elif nums[left] == 0 and nums[right] == 0:
+                    right += 1
+                elif nums[left] != 0 and nums[right] != 0:
+                    left +=1 
+                    right +=1
+                elif nums[left] != 0 and nums[right] == 0:
+                    left += 1 
+                    right += 1
+                
+                
+            
+        
