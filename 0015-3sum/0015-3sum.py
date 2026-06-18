84e0a9eece21e8e4216ab26f206dc34330d3eaa1
Anurag Parla <anuragparla@gmail.com>
2026-06-18 10:39:52 -0400

    Time: 509 ms (85.51%), Space: 22.2 MB (81.34%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,42 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         '''
-        -4 -1 -1 0 1 2 
+        for a triplet to form one of the digit
+        should be negative or at least a 0 
+        duplicate triplets should not be considered
+        in the final result
+        input is always at least 3 elements 
+        need to finall 2 sum pairs for the first digit in the triplet
         '''
-        res = []
-        nums.sort()
-        for i, v in enumerate(nums):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            left, right = i +1, len(nums)-1 
-            while left<right:
-                three_sum = v + nums[left] + nums[right]
-                if three_sum > 0: 
-                    right -= 1 
-                elif three_sum < 0: 
-                    left += 1 
+        def find_two_sum_pairs (nums, j, tgt):
+            k = len(nums) - 1
+            pairs = [] 
+            while j < k:
+                if nums[j] + nums[k] == tgt:
+                    pairs.append([nums[j],nums[k]])
+                    j += 1 
+                    while j < k and nums[j] == nums [j - 1]:
+                        j += 1 
+                elif nums[j] + nums[k] > tgt:
+                    k -= 1
                 else:
-                    res.append([nums[i], nums[left], nums[right]])
-                    left += 1 
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1 
+                    j += 1 
+            return pairs
+
+        if len(nums) < 3:
+            return []
+        res = []
+
+        nums.sort() # inward two pointer will not work without the list being sorted
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break # we cannot form triplets with only +ve numbers
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue # we don't want the first number to be considered again if it duplicates
+            if i < len(nums) - 1:
+                pairs = find_two_sum_pairs(nums, i+1, -nums[i])
+            for pair in pairs:
+                res.append([nums[i]]+pair)
         return res
 
-            
-
-        
         
