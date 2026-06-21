c536be168ef50b9fe83d7a12987ed3df56d04358
Anurag Parla <anuragparla@gmail.com>
2026-06-21 09:09:45 -0400

    Time: 17 ms (25.37%), Space: 19.3 MB (68.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,34 +1,27 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         '''
-        Variable window problem
-        we need to keep left and right as start and end for the window
-        since this problem asks us not have duplicates, i will use a 
-        set to keep track of duplicates 
-        initially both left and right start from 0 
-        hence window size can be calculated as (right - left) + 1 
-        perform a max on that to return the longest 
-        it gets tricky when right pointer points to a character in the set 
-        then you need to move the left pointer and everytime it moves 
-        remove its corresponding element till s[r] is not in the set 
-        when it isn't it will be the updated end point of the window and now 
-        add that to the set and continue till r goes out of bounds 
-        Time: O(n)
-        Space: O(n) as in a string with unique chars, all n chars will be 
-        stored 
+        time: o(N)
+        space: O(N)
+        need a set to keep track of duplicate chars
+        need a max_length
+        need left and right ptrs for variable sliding window
         '''
-        left, right = 0, 0 
-        longestSubString = 0
-        trackDuplicates = set()
-        while right<len(s):
-            while s[right] in trackDuplicates:
-                trackDuplicates.remove(s[left])
+        left, right = 0,0
+        track_unique_chars = set()
+        max_length = 0
+
+        if not s:
+            return 0
+        while right < len(s):
+            while s[right] in track_unique_chars:
+                track_unique_chars.remove(s[left])
                 left += 1 
-            currWindowSize = (right-left) + 1 
-            longestSubString = max(longestSubString,currWindowSize )
-            trackDuplicates.add(s[right])
-            right += 1 
-        return longestSubString
+            track_unique_chars.add(s[right])
+            max_length = max(max_length, right - left + 1)
+            right += 1
+        
+        return max_length
 
 
-       
+        
