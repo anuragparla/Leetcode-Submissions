d119a0e3463609f4a0a0494e32cf9b66d3754409
Anurag Parla <anuragparla@gmail.com>
2026-06-24 23:12:12 -0400

    Time: 0 ms (100%), Space: 19.2 MB (63.06%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,25 +1,27 @@
-#Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummyNode = ListNode()
-        dummyNode.next = head
-        slow = dummyNode
+        '''
+        brute force is to first traverse the entire ll to get the length
+        then you need to find the diff between total length and n 
+        the diff will be the node before the node to be removed. Iterate to that node and then if the node.next is not None then remove it if the node is head then after removing you need to update the head 
+        '''
+        dummy = ListNode(0,head)
+        slow = dummy
         fast = head
 
-        while n>0 and fast: 
-            fast = fast.next 
-            n -= 1
+        while fast and n > 0:
+            fast = fast.next
+            n -= 1 
         
         while fast:
-            slow = slow.next   
-            fast = fast.next  
-
+            slow = slow.next
+            fast = fast.next
         slow.next = slow.next.next
-
-        return dummyNode.next  
+        return dummy.next
         
         
