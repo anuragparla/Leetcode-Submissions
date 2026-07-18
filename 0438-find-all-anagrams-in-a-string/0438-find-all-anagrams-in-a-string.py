8d72c98515dc184117d25e2802037dbd12324c2e
Anurag Parla <anuragparla@gmail.com>
2026-07-18 06:56:54 -0400

    Time: 41 ms (55.89%), Space: 19.6 MB (85.56%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,21 +1,25 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        freq_map_p = dict()
-        for chr in p:
-            freq_map_p[chr] = freq_map_p.get(chr,0)+1
-        freq_map_window = {}
-        left , right = 0,0
-        k = len(p)
+        l = r = 0 
+        freq_ct_p = dict()
+        freq_ct_s = dict()
         res = []
-        while right < len(s):
-            freq_map_window[s[right]] = freq_map_window.get(s[right],0) + 1 
-            if right - left + 1 == k:
-                if freq_map_window == freq_map_p: 
-                    res.append(left)
-                freq_map_window[s[left]] -= 1
-                if freq_map_window[s[left]] == 0:
-                    del freq_map_window[s[left]]
-                left += 1 
-            right += 1 
+        for ch in p:
+             freq_ct_p[ch] =  freq_ct_p.get(ch,0) + 1
+        while r < len(s):
+            freq_ct_s[s[r]] = freq_ct_s.get(s[r],0) + 1
+            if (r - l + 1) == len(p):
+                if freq_ct_s == freq_ct_p:
+                    res.append(l)
+                if s[l] in freq_ct_s:
+                    freq_ct_s[s[l]] -= 1 
+                    if freq_ct_s[s[l]] == 0:
+                        del freq_ct_s[s[l]]
+                l += 1
+            r += 1
         return res
+        
+         
+
+        
         
