5aa20243415696e9f646539962f4d9500f52bb3c
Anurag Parla <anuragparla@gmail.com>
2026-07-20 21:01:23 -0400

    Time: 5 ms (6%), Space: 21.6 MB (7%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,13 +1,27 @@
 class Solution:
 
-    
+    def merge(self,idx,merged,newInterval = None, intervals = None,):
+        #check for overlap and merge
+        if intervals:
+
+            if intervals[idx][0] <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], intervals[idx][1])
+            else:
+                merged.append(intervals[idx])
+        else:
+            if newInterval[0] <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], newInterval[1])
+            else:
+                merged.append(newInterval)
 
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+
         #handle empty list
         if not intervals:
             return [newInterval]
         merged = []
         idx = 1
+
         # if the newInterval has the smallest start value
         if newInterval[0] < intervals[0][0]:
             merged = [newInterval]
@@ -17,28 +31,21 @@ class Solution:
 
         # adding all the intervals up unit new interval
         while idx < len(intervals) and intervals[idx][0] <= newInterval[0]:
-            self.merge(idx,intervals,merged)
+            self.merge(idx,merged,None,intervals)
             idx += 1 
+
         # adding new interval to the result
-        if newInterval[0] <= merged[-1][1]:
-            merged[-1][1] = max(merged[-1][1],newInterval[1])
-        else:
-            merged.append(newInterval)
-        
+        self.merge(idx,merged,newInterval,None)
+
         # add the remaining portion of the intervals to the  result 
-        
         while idx < len(intervals):
-            self.merge(idx,intervals,merged)
+            self.merge(idx,merged,None,intervals)
             idx += 1 
-        
+
         return merged
-    
-    def merge(self,idx,intervals,merged):
-        #check for overlap and merge
-        if intervals[idx][0] <= merged[-1][1]:
-            merged[-1][1] = max(merged[-1][1], intervals[idx][1])
-        else:
-            merged.append(intervals[idx])
+                
+            
+
 
 
 
